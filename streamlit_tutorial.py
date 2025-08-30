9d152b3def2d466ca4499bbfb246e643bf5dd8f0
joesu777 <jswan4123@gmail.com>
2025-08-30 15:42:46 +0700

    Finished streamlit script

--- streamlit_tutorial.py
+++ streamlit_tutorial.py
@@ -14,12 +14,21 @@ cat_vs_dog_model = load_learner("cat-vs-dog.pkl")
 def predict(image):
     img = PILImage.create(image)
     pred_class, pred_idx, outputs = cat_vs_dog_model.predict(img)
-    likehood_is_cat = outputs[1].item()
+    likelihood_is_cat = outputs[1].item()
     #Homework:
     #If the likehood_is_cat is more than 0.9, return "Cat"
     #if it's less than 0.1, then return "Dog"
     #otherwise, return "Not sure... try another picture!"
+    if likelihood_is_cat > 0.9:
+        return "Cat"
+    elif likelihood_is_cat < 0.1:
+        return "Dog"
+    else:
+        return "Not sure... try another picture!"
 
-uploaded_file = st.file_uploader("Gamas upload an image...", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader("Choose an image to upload...", type=["jpg", "png", "jpeg"])
+
 if uploaded_file is not None:
-    st.image(uploaded_file, use_container_width=True)
+    st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
+    prediction = predict(uploaded_file)
+    st.write(prediction)
